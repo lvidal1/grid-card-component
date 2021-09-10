91a6ae700bda3989b16c4227b50a6d26e30bfbfc
Leonardo Vidal <lvidal910@gmail.com>
2021-09-10 04:59:49 -0500

    Small fix

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,7 +1,7 @@
 const Hero = () => {
 	return (
 		<section className="px-4 py-5 my-5 text-center">
-			<h1 className="display-5 fw-bold mt-5 text-white">Leonardo's Tech Blog</h1>
+			<h1 className="display-5 fw-bold mt-5 text-white">Leonardo/s Tech Blog</h1>
 			<div className="col-lg-6 mx-auto">
 				<p className="lead mb-4 text-white">
 					Welcome to the our awesome blog!
