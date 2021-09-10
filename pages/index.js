a8b1807bf63d8b7066a92d76cdc5b2922b8b158d
lvidal1 <lvidal910@gmail.com>
2021-09-10 00:38:00 -0500

    Enabled bs5 deps

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.container}>
+      <main className="container">
         <FilterProvider>
           <CardList />
         </FilterProvider>
